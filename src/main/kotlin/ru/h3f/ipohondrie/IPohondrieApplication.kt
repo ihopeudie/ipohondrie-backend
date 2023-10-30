db4da077a55210e9c99b82d197ba0f5422c77a5b
Aleksey Sverbeev <alexey.sverbeev@aliexpress.ru>
2023-10-31 01:28:55 +0300

    init dev

---
+++ src/main/kotlin/ru/h3f/ipohondrie/IPohondrieApplication.kt
@@ -0,0 +1,11 @@
+package ru.h3f.ipohondrie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IPohondrieApplication
+
+fun main(args: Array<String>) {
+	runApplication<IPohondrieApplication>(*args)
+}
---
+++ src/test/kotlin/ru/h3f/ipohondrie/IPohondrieApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.h3f.ipohondrie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IPohondrieApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
